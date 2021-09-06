00865705edae0abf8b58e1da8b8300a55ade8b88
Roshan Bajgain <you@example.com>
2021-09-06 20:27:23 +0545

    Make close botton disappear using css

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const navToggle = document.querySelector(".menu");
-const closebtn = document.querySelector(".closebtn");
+const closebtn = document.querySelector(".close-btn");
 const links = document.querySelector(".menu-items")
 navToggle.addEventListener("click", function () {
     links.classList.toggle("show-links");
 });
-closebtn.addEventListener('click', function() {
+closebtn.addEventListener("click", function () {
     links.classList.remove('show-links');
 });
