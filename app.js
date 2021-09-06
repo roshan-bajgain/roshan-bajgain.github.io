42b501fe194806954ebbbec2413cdcdce45804af
Roshan Bajgain <you@example.com>
2021-09-06 21:42:15 +0545

    Add js linter

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const navToggle = document.querySelector(".menu");
-const closebtn = document.querySelector(".close-btn");
-const links = document.querySelector(".menu-items")
-navToggle.addEventListener("click", function () {
-    links.classList.toggle("show-links");
+const navToggle = document.querySelector('.menu');
+const closebtn = document.querySelector('.close-btn');
+const links = document.querySelector('.menu-items');
+navToggle.addEventListener('click', () => {
+  links.classList.toggle('show-links');
 });
-closebtn.addEventListener("click", function () {
-    links.classList.remove('show-links');
+closebtn.addEventListener('click', () => {
+  links.classList.remove('show-links');
 });
