a724704406c61d0b53ae3fb55177fe0ee92a8d6f
Roshan Bajgain <you@example.com>
2021-09-07 19:40:55 +0545

    Add js to show pop up for project

--- app.js
+++ app.js
@@ -13,3 +13,31 @@ navs.forEach((a) => {
     links.classList.remove('show-links');
   });
 });
+const myprojects = [
+  {
+    name: 'Tonic', detailDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem voluptates, quaerat in, deserunt cumque odio alias expedita quasi distinctio minima saepe optio, doloribus nisi. Numquam ratione vitae quas sed asperiores dicta voluptatum maiores praesentium ab ad, animi commodi ea. Sit?', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', github: 'https://www.github.com/Hectors096', liveDemoLink: 'https://hector096.github.io/My-Portfolio/', imgUrl: 'images/SnapshotPortfolio.png',
+  },
+  {
+    name: 'Multi Project', detailDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem voluptates, quaerat in, deserunt cumque odio alias expedita quasi distinctio minima saepe optio, doloribus nisi. Numquam ratione vitae quas sed asperiores dicta voluptatum maiores praesentium ab ad, animi commodi ea. Sit?', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', github: 'https://www.github.com/Hectors096', liveDemoLink: 'https://hector096.github.io/My-Portfolio/', imgUrl: 'images/SnapshotPortfolio2.png',
+  },
+  {
+    name: 'Tonic', detailDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem voluptates, quaerat in, deserunt cumque odio alias expedita quasi distinctio minima saepe optio, doloribus nisi. Numquam ratione vitae quas sed asperiores dicta voluptatum maiores praesentium ab ad, animi commodi ea. Sit?', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', github: 'https://www.github.com/Hectors096', liveDemoLink: 'https://hector096.github.io/My-Portfolio/', imgUrl: 'images/SnapshotPortfolio3.png',
+  },
+  {
+    name: 'Multi Project', detailDescription: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem voluptates, quaerat in, deserunt cumque odio alias expedita quasi distinctio minima saepe optio, doloribus nisi. Numquam ratione vitae quas sed asperiores dicta voluptatum maiores praesentium ab ad, animi commodi ea. Sit?', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', github: 'https://www.github.com/Hectors096', liveDemoLink: 'https://hector096.github.io/My-Portfolio/', imgUrl: 'images/SnapshotPortfolio4.png',
+  },
+];
+
+const popUpProject = (popUpdata) => {
+  const popUpItem = `<div id="popUp-view">
+     <div class="popUp-card">
+     <img src="x.svg" alt="close" id="popUp-Close" onclick="popupClose();">
+     <h2 class="card-title">${popUpdata.name}</h2>
+         <div class="work-detail-box">
+             <ul>
+                 <li class=" work-detail canopy">CANOPY </li>
+                 <li class="work-detail work-text">Back End Dev </li>
+                 <li class="work-detail work-text">2015</li>
+             </ul>
+         </div>`
+}
